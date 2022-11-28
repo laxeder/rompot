4d658a6868e24a9d6b64455d0fc43361245aea9a
laxeder <laxederbr@gmail.com>
2022-11-28 18:57:23 -0300

    update lib

--- lib/buttons/ContactMessage.d.ts
+++ lib/buttons/ContactMessage.d.ts
@@ -3,7 +3,7 @@ import { Chat } from "../models/Chat";
 import { User } from "../models/User";
 export declare class ContactMessage extends Message {
     contacts: User[];
-    constructor(chat: Chat, text: string, user: User | User[], mention?: Message, id?: string);
+    constructor(chat: Chat, text: string, contacts: User | User[], mention?: Message, id?: string);
     /**
      * * Define o usuário do contato
      * @param user
