6ef096aacbb2eb82a6124bf02c1977482a77c2fe
laxeder <laxederbr@gmail.com>
2022-11-28 15:58:46 -0300

    add media message

--- src/models/ImageMessage.ts
+++ src/models/ImageMessage.ts
@@ -3,27 +3,9 @@ import { Message } from "@models/Message";
 import { Chat } from "@models/Chat";
 
 export class ImageMessage extends MediaMessage {
-  private _image: Buffer | string;
-
-  constructor(chat: Chat, text: string, _image: Buffer | string, mention?: Message, id?: string) {
+  constructor(chat: Chat, text: string, image: any, mention?: Message, id?: string) {
     super(chat, text, mention, id);
 
-    this._image = _image;
-  }
-
-  /**
-   * * Define a imagem da mensagem
-   * @param image
-   */
-  public setImage(image: Buffer | string) {
-    this._image = image;
-  }
-
-  /**
-   * * Obtem a imagem da mensagem
-   * @returns
-   */
-  public async getImage() {
-    return await this.getStream(this._image);
+    this.setImage(image);
   }
 }
--- src/models/MediaMessage.ts
+++ src/models/MediaMessage.ts
@@ -3,6 +3,11 @@ import { Chat } from "@models/Chat";
 
 export class MediaMessage extends Message {
   private _getStream?: Function;
+  private _image?: any;
+  private _video?: any;
+  private _audio?: any;
+
+  public isGIF: boolean = false;
 
   constructor(chat: Chat, text: string, mention?: Message, id?: string) {
     super(chat, text, mention, id);
@@ -22,9 +27,75 @@ export class MediaMessage extends Message {
    * @returns
    */
   public async getStream(stream: any) {
-    //TODO: Obter base64
     if (stream instanceof Buffer) return stream;
     if (this._getStream) return await this._getStream(stream);
     return Buffer.from("");
+  }
+
+  /**
+   * * Define a imagem da mensagem
+   * @param image
+   */
+  public setImage(image: any) {
+    this._image = image;
+  }
+
+  /**
+   * * Obtem a imagem da mensagem
+   * @returns
+   */
+  public async getImage() {
+    if (!!!this._image) return undefined;
+    return await this.getStream(this._image);
+  }
+
+  /**
+   * * Define o video da mensagem
+   * @param video
+   */
+  public setVideo(video: any) {
+    this._video = video;
+  }
+
+  /**
+   * * Obtem o video da mensagem
+   * @returns
+   */
+  public async getVideo() {
+    if (!!!this._video) return undefined;
+    return await this.getStream(this._video);
+  }
+
+  /**
+   * * Define o audio da mensagem
+   * @param audio
+   */
+  public setAudio(audio: any) {
+    this._audio = audio;
+  }
+
+  /**
+   * * Obtem o audio da mensagem
+   * @returns
+   */
+  public async getAudio() {
+    if (!!!this._audio) return undefined;
+    return await this.getStream(this._audio);
+  }
+
+  /**
+   * * Define se a imagem é um GIF
+   * @param gif
+   */
+  public setIsGIF(gif: boolean) {
+    this.isGIF = gif;
+  }
+
+  /**
+   * * Retorna se a imagem é um GIF
+   * @returns
+   */
+  public getIsGIF(): boolean {
+    return this.isGIF;
   }
 }
