4dc501056dcec6eb8e1be4a7a1eb1092f095be5e
laxeder <laxederbr@gmail.com>
2022-12-02 20:36:17 -0300

    update example

--- example/index.js
+++ example/index.js
@@ -1,21 +1,6 @@
 const { Bot, WhatsAppBot, Message, logger, Commands, Command } = require("rompot");
 
-const hello = new Command("hello", "Manda um simples Hello");
-hello.setSend("Hello There!");
-
-const date = new Command(["date", "dt", "data"]);
-date.setExecute((message) => {
-  const bot = message.getBot();
-  bot?.send(new Message(message.chat, `Data: ${new Date()}`));
-});
-
 const bot = new Bot(new WhatsAppBot());
-
-const commands = new Commands({ hello, date }, bot);
-commands.setPrefix("/");
-
-bot.setCommands(commands);
-bot.build("./example/auth");
 
 bot.on("connection", (update) => {
   if (update.action == "open") {
@@ -36,27 +21,84 @@ bot.on("connection", (update) => {
 });
 
 bot.on("message", async (message) => {
-  console.log(`New message in ${message.chat.id}`);
+  logger.info(`New message in ${message.chat.id}`);
 });
 
 bot.on("bot-message", (message) => {
-  console.log(`Send message to ${message.user.phone}`);
+  logger.info(`Send message to ${message.user.phone}`);
 });
 
 bot.on("chat", (chat) => {
-  console.log(`New chat: ${chat.id}`);
+  logger.info(`New chat: ${chat.id}`);
 });
 
 bot.on("member", (member) => {
   if (member.action == "add") {
-    console.log(`Number ${member.user.phone} joined group ${member.chat.id}`);
+    logger.info(`Number ${member.user.phone} joined group ${member.chat.id}`);
   }
 
   if (member.action == "remove") {
-    console.log(`Member group ${member.chat.id} left`);
+    logger.info(`Member group ${member.chat.id} left`);
   }
 });
 
 bot.on("error", (err) => {
-  console.log("Um erro ocorreu:", err);
+  logger.error("Um erro ocorreu:", err);
 });
+
+const hello = new Command("hello", "Manda um simples Hello");
+hello.setSend("Hello There!");
+
+const date = new Command(["date", "dt", "data"]);
+date.setExecute((message) => {
+  const bot = message.getBot();
+  bot?.send(new Message(message.chat, `Data: ${new Date()}`));
+});
+
+const ban = new Command(["ban", "expulse"]);
+ban.setExecute((message) => {
+  if (message.chat.type !== "group") {
+    return message.reply("Apenas é possível banir membros em grupos");
+  }
+
+  if (!message.chat.getMember(message.user)?.isAdmin) {
+    return message.reply("Vocẽ não tem permissão para executar esse comando");
+  }
+
+  if (!message.chat.getMember(bot.getBot().user)?.isAdmin) {
+    return message.reply("Eu não tenho permissão para executar esse comando");
+  }
+
+  if (message.mentions.length < 1) {
+    return message.reply("Vocẽ precisa mencionar alguem para que ela possa ser banida");
+  }
+
+  bot.removeMember(message.chat, new User(message.mentions[0] || ""));
+});
+
+const add = new Command(["add"]);
+add.setExecute((message) => {
+  if (message.chat.type !== "group") {
+    return message.reply("Apenas é possível adicionar membros em grupos");
+  }
+
+  if (!message.chat.getMember(message.user)?.isAdmin) {
+    return message.reply("Vocẽ não tem permissão para executar esse comando");
+  }
+
+  if (!message.chat.getMember(bot.getBot().user)?.isAdmin) {
+    return message.reply("Eu não tenho permissão para executar esse comando");
+  }
+
+  if (message.mentions.length < 1) {
+    return message.reply("Vocẽ precisa mencionar alguem para que ela possa ser adicionada");
+  }
+
+  bot.addMember(message.chat, new User(message.mentions[0] || ""));
+});
+
+const commands = new Commands({ hello, date, ban, add }, bot);
+commands.setPrefix("/");
+
+bot.setCommands(commands);
+bot.build("./example/auth", { disableAutoCommand: false, disableAutoRead: false });
