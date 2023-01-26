594369243877185f70180ecc495142bda9ef6051
laxeder <laxederbr@gmail.com>
2023-01-26 16:10:27 -0300

    add bot interface

--- src/models/Bot.ts
+++ src/models/Bot.ts
@@ -245,3 +245,84 @@ export class Bot extends Emmiter {
   public async setProfile(image: Buffer, id?: Chat | string): Promise<any> {}
   public async getProfile(id?: Chat | User | string): Promise<any> {}
 }
+
+export interface BotBase {
+  //? ************ CONNECTION ************
+
+  connect(config: ConnectionConfig): Promise<void>;
+  reconnect(config: ConnectionConfig): Promise<void>;
+  stop(reason: any): Promise<void>;
+
+  //? ************** MESSAGE *************
+
+  sendMessage(message: Message): Promise<Message>;
+  removeMessage(message: Message): Promise<void>;
+  deleteMessage(message: Message): Promise<void>;
+
+  //? ************** STATUS **************
+
+  sendStatus(status: Status): Promise<Status>;
+
+  //? *************** BOT ***************
+
+  getBotProfile(): Promise<Buffer | null>;
+  setBotProfile(image: Buffer): Promise<void>;
+
+  getBotName(): Promise<string>;
+  setBotName(name: string): Promise<void>;
+
+  getBotDescription(): Promise<string>;
+  setBotDescription(description: string): Promise<string>;
+
+  getChatProfile(chatId: string): Promise<Buffer | null>;
+  setChatProfile(chatId: string, image: Buffer): Promise<void>;
+
+  //? *************** CHAT **************
+
+  addChat(chat: Chat): Promise<void>;
+  removeChat(chatId: string): Promise<void>;
+
+  createChat(chat: Chat): Promise<void>;
+  leaveChat(chatId: string): Promise<void>;
+
+  getChat(chatId: string): Promise<Chat | null>;
+  setChat(chat: Chat): Promise<void>;
+
+  getChats(): Promise<{ [chatId: string]: Chat }>;
+  setChats(chats: { [chatId: string]: Chat }): Promise<void>;
+
+  getChatName(chatId: string): Promise<string>;
+  setChatName(chatId: string, name: string): Promise<void>;
+
+  getChatDescription(chatId: string): Promise<string>;
+  setChatDescription(chatId: string, description: string): Promise<string>;
+
+  getUserProfile(userId: string): Promise<Buffer | null>;
+  setUserProfile(userId: string, image: Buffer): Promise<void>;
+
+  //? *************** USER **************
+
+  addUser(user: User): Promise<void>;
+  removeUser(userId: string): Promise<void>;
+
+  unblockUser(userId: string): Promise<void>;
+  blockUser(userId: string): Promise<void>;
+
+  getUser(userId: string): Promise<User | null>;
+  setUser(user: User): Promise<void>;
+
+  getUsers(): Promise<{ [userId: string]: User }>;
+  setUsers(users: { [userId: string]: User }): Promise<void>;
+
+  getUserName(userId: string): Promise<string>;
+  setUserName(userId: string, name: string): Promise<void>;
+
+  getUserDescription(userId: string): Promise<string>;
+  setUserDescription(userId: string, description: string): Promise<string>;
+}
+
+export class BotModule {
+  constructor(bot: Bot) {}
+}
+
+export interface BotModule {}
---
+++ src/utils/PromiseMessages.ts
@@ -0,0 +1,81 @@
+import { Message } from "@messages/Message";
+
+export type PromiseMessage = {
+  [chatId: string]: {
+    stopRead: boolean;
+    ignoreMessageFromMe: boolean;
+    ignoreMessages: Message[];
+    resolve(message: Message): void;
+  }[];
+};
+
+export default class PromiseMessages {
+  public promisses: PromiseMessage = {};
+
+  constructor(promisses: PromiseMessage = {}) {
+    this.promisses = promisses;
+  }
+
+  /**
+   * * Adiciona uma nova promessa de mensagem
+   * @param chatId Sala de bate-papo que irá receber a mensagem
+   * @param ignoreMessageFromMe Ignora a mensagem se quem enviou foi o próprio bot
+   * @param stopRead Para de ler a mensagem no evento
+   * @param ignoreMessages Não resolve a promessa se a mensagem recebida é a mesma escolhida
+   * @returns
+   */
+  public addPromiseMessage(
+    chatId: string,
+    ignoreMessageFromMe: boolean = true,
+    stopRead: boolean = true,
+    ...ignoreMessages: Message[]
+  ): Promise<Message> {
+    if (!this.promisses.hasOwnProperty(chatId)) {
+      this.promisses[chatId] = [];
+    }
+
+    return new Promise((resolve) => {
+      this.promisses[chatId].push({
+        stopRead,
+        ignoreMessageFromMe,
+        ignoreMessages,
+        resolve,
+      });
+    });
+  }
+
+  /**
+   * * Resolve promessas de mensagens que estão esperando ser recebidas
+   * @param message
+   * @returns Retorna se é para continuar a leitura da mensagem na sala de bate-papo ou não
+   */
+  public resolvePromiseMessages(message: Message): boolean {
+    const chatId: string = message.chat.id!;
+    var stop: boolean = false;
+
+    if (!!!chatId || !this.promisses.hasOwnProperty(chatId)) return stop;
+
+    this.promisses[chatId].forEach((prom, index) => {
+      if (message.fromMe && prom.ignoreMessageFromMe) return;
+
+      let cont: boolean = true;
+
+      for (const m of prom.ignoreMessages) {
+        if (m.id == message.id) {
+          cont = false;
+          break;
+        }
+      }
+
+      if (!cont) return;
+
+      prom.resolve(message);
+
+      this.promisses[chatId].splice(index, 1);
+
+      if (prom.stopRead) stop = true;
+    });
+
+    return stop;
+  }
+}
