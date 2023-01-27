f0a272a0a71ff33c712ddbb1b48a9692edcaf347
laxeder <laxederbr@gmail.com>
2023-01-27 10:04:30 -0300

    remove link

--- example/index.ts
+++ example/index.ts
@@ -1,4 +1,4 @@
-import { WhatsAppBot, Message, Commands, Command, User } from "rompot";
+import { WhatsAppBot, Message, Commands, Command, User } from "../src/index";
 
 const bot = new WhatsAppBot({
   disableAutoCommand: false,
