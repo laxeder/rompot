dc05ad91dc82940b46c548ac18f430b1a3facfc1
laxeder <laxederbr@gmail.com>
2023-01-30 12:40:13 -0300

    update chat module

--- src/modules/Chat.ts
+++ src/modules/Chat.ts
@@ -1,116 +1,112 @@
-import { ChatTypes } from "../types/Chat";
-import { User } from "@modules/User";
-import { Bot } from "@modules/Bot";
+import UserInterface from "@interfaces/UserInterface";
+import ChatInterface from "@interfaces/ChatInterface";
 
-var bot: Bot;
+import BotBase from "@modules/BotBase";
 
-export class Chat {
+import { setBotProperty } from "@utils/bot";
+import { getUserId } from "@utils/Marshal";
+
+import { ChatModule, ChatTypes } from "../types/Chat";
+import { BotModule } from "../types/BotModule";
+import { Users } from "../types/User";
+
+export default class Chat implements ChatModule {
   public id: string;
+  public type: ChatTypes;
   public name: string;
   public description: string;
-  public type: ChatTypes;
-  public members: { [key: string]: User } = {};
+  public profile: Buffer;
+  public users: Users;
 
-  constructor(id: string, name?: string, description?: string, type?: ChatTypes) {
+  get bot(): BotModule {
+    return new BotBase();
+  }
+
+  constructor(id: string, type?: ChatTypes, name?: string, description?: string, profile?: Buffer, users?: Users) {
     this.id = id;
+    this.type = type || "pv";
     this.name = name || "";
     this.description = description || "";
-    this.type = type || "pv";
+    this.profile = Buffer.from("") || "";
+    this.users = users || {};
   }
 
-  /**
-   * * Define o nome da sala de bate-papo
-   * @param name
-   */
-  public async setName(name: string) {
+  public async setName(name: string): Promise<void> {
     this.name = name;
 
-    await bot?.setChatName(this, name);
+    await this.bot.setChatName(this, name);
+  }
+
+  public async getName(): Promise<string> {
+    return this.bot.getChatName(this);
+  }
+
+  public async getDescription(): Promise<string> {
+    return this.bot.getChatDescription(this);
+  }
+
+  public async setDescription(description: string): Promise<void> {
+    this.description = description;
+
+    return this.bot.setChatDescription(this, description);
+  }
+
+  public async getProfile(): Promise<Buffer> {
+    return this.bot.getChatProfile(this);
+  }
+
+  public async setProfile(image: Buffer): Promise<void> {
+    this.profile = image;
+
+    return this.bot.setChatProfile(this, image);
+  }
+
+  public async IsAdmin(user: UserInterface | string): Promise<boolean> {
+    const admins = await this.bot.getChatAdmins(this);
+
+    return admins.hasOwnProperty(getUserId(user));
+  }
+
+  public async IsLeader(user: UserInterface | string): Promise<boolean> {
+    const leader = await this.bot.getChatLeader(this);
+
+    return leader.id == getUserId(user);
+  }
+
+  public async getAdmins(): Promise<Users> {
+    return this.bot.getChatAdmins(this);
+  }
+
+  public async addUser(user: UserInterface | string): Promise<void> {
+    return this.bot.addUserInChat(this, user);
+  }
+
+  public async removeUser(user: UserInterface | string): Promise<void> {
+    return this.bot.removerUserInChat(this, user);
+  }
+
+  public async promote(user: UserInterface | string): Promise<void> {
+    return this.bot.promoteUserInChat(this, user);
+  }
+
+  public async demote(user: UserInterface | string): Promise<void> {
+    return this.bot.demoteUserInChat(this, user);
+  }
+
+  public async leave(): Promise<void> {
+    return this.bot.leaveChat(this);
   }
 
   /**
-   * * Define a descrição da sala de bate-papo
-   * @param desc
+   * * Injeta a interface no modulo
+   * @param bot Bot que irá executar os métodos
+   * @param chat Interface da sala de bate-papo
    */
-  public async setDescription(desc: string) {
-    this.description = this.description;
+  public static Inject<ChatIn extends ChatInterface>(bot: BotModule, chat: ChatIn): ChatIn & ChatModule {
+    const module = new Chat(chat.id, chat.type, chat.name, chat.description, chat.profile, chat.users);
 
-    await bot?.setDescription(desc, this);
-  }
+    setBotProperty(bot, module);
 
-  /**
-   * * Define o bot da sala de bate-papo
-   * @param bot
-   */
-  public setBot(bot: Bot) {
-    bot = bot;
-  }
-
-  /**
-   * * Retorna o bot da sala de bate-papo
-   * @returns
-   */
-  public getBot(): Bot {
-    return bot;
-  }
-
-  /**
-   * * Adiciona um novo membro a sala de bate-papo
-   * @param member
-   */
-  public async addMember(member: User | string) {
-    if (!(member instanceof User)) member = new User(`${member}`);
-
-    await bot?.addMember(this, member);
-
-    this.members[member.id] = member;
-  }
-
-  /**
-   * * Remove um membro da sala de bate-papo
-   * @param member
-   * @returns
-   */
-  public async removeMember(member: User | string) {
-    if (!(member instanceof User)) member = new User(`${member}`);
-
-    await bot?.removeMember(this, member);
-
-    delete this.members[member.id];
-  }
-
-  /**
-   * * Retorna um membro da sala de bate-papo
-   * @param member
-   * @returns
-   */
-  public getMember(member: User | string): User | undefined {
-    if (!(member instanceof User)) member = new User(`${member}`);
-
-    return this.members[member.id];
-  }
-
-  /**
-   * * Retorna a imagem do chat
-   * @returns
-   */
-  public async getProfile(): Promise<any> {
-    return await bot?.getProfile(this);
-  }
-
-  /**
-   * * Define a imagem da sala de bate-papo
-   * @param image
-   */
-  public async setProfile(image: Buffer): Promise<any> {
-    return await bot?.setProfile(image, this);
-  }
-
-  /**
-   * * Sai da sala de bate-papo
-   * @returns
-   */
-  public async leave() {
-    return await bot?.leaveChat(this);
+    return { ...chat, ...module };
   }
 }
