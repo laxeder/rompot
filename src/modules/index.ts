7ea1f4aaf8e640540d718056f4b16da08848f74d
laxeder <laxederbr@gmail.com>
2023-01-31 12:56:47 -0300

    remove index

--- src/modules/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "@modules/Commands";
-export * from "@modules/Command";
-export * from "@modules/Status";
-export * from "@modules/Chat";
-export * from "@modules/User";
-export * from "@modules/Bot";
