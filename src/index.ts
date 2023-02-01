b8ab8c68489aae71ff7a87a21753fe154a79927a
laxeder <laxederbr@gmail.com>
2023-02-01 17:37:23 -0300

    add exports

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+import ConnectionConfig from "@config/ConnectionConfig";
+
+import UserInterface from "@interfaces/UserInterface";
+import ChatInterface from "@interfaces/ChatInterface";
+import BotInterface from "@interfaces/BotInterface";
+import BotControl from "@interfaces/BotControl";
+
+import LocationMessage from "@messages/LocationMessage";
+import ContactMessage from "@messages/ContactMessage";
+import ButtonMessage from "@messages/ButtonMessage";
+import ImageMessage from "@messages/ImageMessage";
+import VideoMessage from "@messages/VideoMessage";
+import MediaMessage from "@messages/MediaMessage";
+import ListMessage from "@messages/ListMessage";
+import Message from "@messages/Message";
+
+import { Commands } from "@modules/Commands";
+import { Command } from "@modules/Command";
+import { BuildBot } from "@modules/Bot";
+import Chat from "@modules/Chat";
+import User from "@modules/User";
+
+import Emmiter, { EventsEmitter } from "@utils/Emmiter";
+import getImageURL from "@utils/getImageURL";
+import PromiseMessages, { PromiseMessage } from "@utils/PromiseMessages";
+import sleep from "@utils/sleep";
+import WaitCallBack from "@utils/WaitCallBack";
+
+export { ConnectionConfig };
+
+export { UserInterface, ChatInterface, BotInterface, BotControl };
+export * from "@interfaces/MessagesInterfaces";
+
+export * from "./types/Connection";
+export * from "./types/BotModule";
+export * from "./types/Messages";
+export * from "./types/Member";
+export * from "./types/Chat";
+export * from "./types/User";
+
+export { ButtonMessage, ContactMessage, ImageMessage, VideoMessage, MediaMessage, Message, ListMessage, LocationMessage };
+
+export { BuildBot, Chat, User, Command, Commands };
+
+export default BuildBot;
+
+export { Emmiter, EventsEmitter, PromiseMessages, PromiseMessage, WaitCallBack, sleep, getImageURL };
+export * from "@utils/Marshal";
+export * from "@utils/error";
+export * from "@utils/bot";
