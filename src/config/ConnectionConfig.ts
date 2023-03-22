7c16ada90892e051775602c0939718a98cb84291
laxeder <laxederbr@gmail.com>
2023-03-22 14:36:42 -0300

    remove config

--- src/config/ConnectionConfig.ts
+++ src/config/ConnectionConfig.ts
@@ -3,14 +3,12 @@ import CommandConfig, { DefaultCommandConfig } from "@config/CommandConfig";
 export interface ConnectionConfig {
   commandConfig: CommandConfig;
   disableAutoCommand?: boolean;
-  printQRInTerminal?: boolean;
   disableAutoTyping?: boolean;
   disableAutoRead?: boolean;
 }
 
 export const DefaultConnectionConfig = {
   commandConfig: DefaultCommandConfig,
-  printQRInTerminal: true,
   disableAutoCommand: false,
   disableAutoTyping: false,
   disableAutoRead: false,
