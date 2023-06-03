af1982ec2dc3c8d36e7c61f17e5705cb7d9ff369
laxeder <laxederbr@gmail.com>
2023-06-03 09:05:36 -0300

    (build) v1.5.6

--- lib/config/Api.d.ts
+++ lib/config/Api.d.ts
@@ -1,1 +1,1 @@
-export declare const ROMPOT_VERSION = "1.5.3";
+export declare const ROMPOT_VERSION = "1.5.6";
--- src/config/Api.ts
+++ src/config/Api.ts
@@ -1,1 +1,1 @@
-export const ROMPOT_VERSION = "1.5.3";
+export const ROMPOT_VERSION = "1.5.6";
