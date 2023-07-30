631528a1669dc4e0eea03132b45a013d0b7a8c0e
laxeder <laxederbr@gmail.com>
2023-07-30 11:18:22 -0300

    feat: rompot exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import Client from "@modules/Client";
+import Client from "@modules/client/models/Client";
 
 import { WhatsAppConvertMessage } from "@wa/WAConvertMessage";
 import ConfigWAEvents from "@wa/ConfigWAEvents";
@@ -7,22 +7,17 @@ import WhatsAppBot from "@wa/WhatsAppBot";
 
 export * from "@config/index";
 
-export * from "@enums/index";
-
-export * from "@interfaces/index";
-
 export * from "@messages/index";
 
 export * from "@modules/index";
 
 export * from "@utils/index";
 
-export * from "./types";
-
 export { MultiFileAuthState, WhatsAppBot, WhatsAppConvertMessage, ConfigWAEvents };
 export * from "@wa/WAModules";
 export * from "@wa/WAStatus";
 export * from "@wa/WAModule";
-export * from "@wa/WATypes";
+
+export * from "rompot-base";
 
 export default Client;
