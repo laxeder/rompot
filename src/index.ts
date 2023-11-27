6cc07d551c448988a09fa2fe5949d7577ef34695
laxeder <laxederbr@gmail.com>
2023-11-27 10:22:56 -0300

    Add wa exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
 import Client from "./client/Client";
 
-import ConfigWAEvents from "./wa/ConfigWAEvents";
-import { MultiFileAuthState } from "./wa/Auth";
-import WhatsAppBot from "./wa/WhatsAppBot";
-
-export { MultiFileAuthState, WhatsAppBot, ConfigWAEvents };
-
 export * from "./bot/index";
 export * from "./chat/index";
 export * from "./client/index";
@@ -14,7 +8,6 @@ export * from "./configs/index";
 export * from "./messages/index";
 export * from "./user/index";
 export * from "./utils/index";
-export * from "./wa/WAStatus";
-export * from "./wa/WAModule";
+export * from "./wa/index";
 
 export default Client;
---
+++ src/wa/index.ts
@@ -0,0 +1,11 @@
+import ConfigWAEvents from "./ConfigWAEvents";
+import { ConvertToWAMessage } from "./ConvertToWAMessage";
+import { ConvertWAMessage } from "./ConvertWAMessage";
+import WhatsAppBot from "./WhatsAppBot";
+
+export * from "./Auth";
+export * from "./ID";
+export * from "./WAModule";
+export * from "./WAStatus";
+
+export { ConfigWAEvents, ConvertToWAMessage, ConvertWAMessage, WhatsAppBot };
