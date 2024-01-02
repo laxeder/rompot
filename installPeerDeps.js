be4af147e9acf556c3d0e2f419654941d6fec935
laxeder <laxederbr@gmail.com>
2024-01-02 10:46:49 -0300

    Fix install peer deps

--- installPeerDeps.js
+++ installPeerDeps.js
@@ -2,4 +2,6 @@ const { execSync } = require("child_process");
 
 const peerDeps = Object.entries(require("./package.json").peerDependencies);
 
-peerDeps.forEach(([dep, version]) => execSync(`npm install -g ${dep}@${version}`));
+for (const [dep, version] of peerDeps) {
+  execSync(`npm install -g ${dep}@${version}`);
+}
