e6f13c619432cc997deacb29cbf1c256a96494c1
laxeder <laxederbr@gmail.com>
2024-01-03 16:18:39 -0300

    release: v1.9.5

--- installPeerDeps.js
+++
@@ -1,7 +0,0 @@
-const { execSync } = require("child_process");
-
-const peerDeps = Object.entries(require("./package.json").peerDependencies);
-
-for (const [dep, version] of peerDeps) {
-  execSync(`npm install -g ${dep}@${version}`);
-}
